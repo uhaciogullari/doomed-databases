45f0601e20662a387758951fdf059e2afe76de98
Ufuk Hacıoğulları <ufukhaciogullari@gmail.com>
2017-11-12 17:20:20 +0100

    Added test project

---
+++ test/DoomedDatabases.Tests/CreateDatabaseTests.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace DoomedDatabases.Tests
+{
+    [Collection("Database")]
+    public class CreateDatabaseTests
+    {
+        [Fact]
+        public void CreateAndDropDatabase()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ test/DoomedDatabases.Tests/DatabaseFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using DoomedDatabases.Postgres;
+using Xunit;
+
+namespace DoomedDatabases.Tests
+{
+    public class DatabaseFixture : IDisposable
+    {
+        private readonly ITestDatabase testDatabase;
+
+        public DatabaseFixture()
+        {
+            testDatabase = new TestDatabaseBuilder().WithConnectionString("User ID=integration_test_user;Password=432423ff;Server=localhost;Database=postgres;").Build();
+            testDatabase.Create();
+        }
+
+        public void Dispose()
+        {
+            testDatabase.Drop();
+        }
+    }
+
+    [CollectionDefinition("Database")]
+    public class DatabaseCollectionFixture : ICollectionFixture<DatabaseFixture>
+    {
+    }
+}
